fdccac72136e702eeed7b53f51f5dcdd799cd970
Todd Burnside <canoebum@gmail.com>
2017-03-09 19:39:18 -0800

    Fix test after cats.

--- src/test/scala/services/PeakServiceSpec.scala
+++ src/test/scala/services/PeakServiceSpec.scala
@@ -7,7 +7,6 @@ import org.http4s.{Method, Request, Status, Uri}
 import org.specs2.matcher.ResultMatchers
 import org.specs2.mutable.Specification
 import repositories.PeakRepository
-
 import fs2.{Stream, Task}
 import cats.implicits._
 
@@ -17,15 +16,15 @@ trait PeakRepo extends PeakRepository {
   val newPeak = new NewPeak((), "Name", 1, "OR", "Lane", "themap", 1000, Location(1.1, 2.2))
   val peak = new Peak(1, "Name", 1, "OR", "Lane", "themap", 1000, Location(1.1, 2.2))
   val somePV = peak.some
-  val eSomePV = somePV.right[Throwable]
+  val eSomePV = somePV.asRight[Throwable]
 
 
-  def find(minElev: Int): Process[Task, Peak] = ???
+  def find(minElev: Int): Stream[Task, Peak] = ???
   def findOne(id: Int) = Task.now(eSomePV)
-  def insert(newPeak: NewPeak) = Task.now(peak.right)
+  def insert(newPeak: NewPeak) = Task.now(peak.asRight)
 }
 
-object PeakServiceSpec extends Specification with TaskMatchers with ResultMatchers {
+object PeakServiceSpec extends Specification with ResultMatchers {
 
   "A simple test to get started" >> {
     "Get should be successful" >> {
@@ -36,8 +35,8 @@ object PeakServiceSpec extends Specification with TaskMatchers with ResultMatche
       var peakSvc = new PeakService(repo, elevProvider)
       val request = Request(Method.GET, Uri(path = "/3"))
       val responseTask = peakSvc.service.run(request)
-      val response = responseTask.unsafePerformSync
-      response.status must_== Status.Ok
+      val response = responseTask.unsafeRun
+      response.orNotFound.status must_== Status.Ok
     }
   }
 }
