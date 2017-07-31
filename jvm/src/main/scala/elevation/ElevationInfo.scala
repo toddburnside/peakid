cab235459a03b35bfa3e2eeedb708cc3e66d71e1
Todd Burnside <canoebum@gmail.com>
2017-07-30 19:18:54 -0700

    Refactor the elevation provider to isolate GoogleResponse processing.

--- jvm/src/main/scala/elevation/ElevationInfo.scala
+++ jvm/src/main/scala/elevation/ElevationInfo.scala
@@ -1,6 +1,6 @@
 package elevation
 
-import cats.implicits._, cats.data._
+import cats.data._
 import fs2.Task
 
 object ElevationInfo {
@@ -8,21 +8,5 @@ object ElevationInfo {
   def getElevation(
       lon: Double,
       lat: Double): Reader[ElevationProvider, Task[Throwable Either Int]] =
-    Reader(_.getElevation(lon, lat).map {
-      task =>
-        task
-          .flatMap { response =>
-            response.results match {
-              case h :: t => (h.elevation.round.toInt).asRight // we have data
-              case _ => {
-                // don't have data, so use the error_message if there is one
-                val msg = response.error_message.getOrElse("Unkown Error.")
-                (new Exception(msg)).asLeft
-              }
-            }
-          }
-          .leftMap(exc =>
-            new Exception(
-              "Error getting elevation from Google: " + exc.getMessage))
-    })
+    Reader(_.getElevation(lon, lat))
 }
--- jvm/src/main/scala/elevation/ElevationProvider.scala
+++ jvm/src/main/scala/elevation/ElevationProvider.scala
@@ -6,10 +6,10 @@ import models.GoogleElevationResponse
 import org.http4s.client.Client
 
 import fs2.Task
+import cats.implicits._
 
 trait ElevationProvider {
-  def getElevation(lon: Double,
-                   lat: Double): Task[Throwable Either GoogleElevationResponse]
+  def getElevation(lon: Double, lat: Double): Task[Throwable Either Int]
 }
 
 class GoogleElevationProvider(key: String, client: Client)
@@ -18,11 +18,31 @@ class GoogleElevationProvider(key: String, client: Client)
   implicit def circeJsonDecoder[A](implicit decoder: Decoder[A]) =
     org.http4s.circe.jsonOf[A]
 
-  override def getElevation(
-      lon: Double,
-      lat: Double): Task[Throwable Either GoogleElevationResponse] = {
+  override def getElevation(lon: Double,
+                            lat: Double): Task[Throwable Either Int] = {
     val uri =
       s"https://maps.googleapis.com/maps/api/elevation/json?locations=$lat,$lon&key=$key"
-    client.expect[GoogleElevationResponse](uri).attempt
+    val responseET = client.expect[GoogleElevationResponse](uri).attempt
+    // might be a better way than map + flatMap...
+    responseET
+      .map { responseE => // get the Either from within the Task
+        responseE
+          .flatMap { r => // The response from Google (assuming no error)
+            // now, see if we have data in the response
+            r.results match {
+              // we only asked for one elevation, so all we care about is the first element
+              case h :: t => (h.elevation.round.toInt).asRight
+              case _ => {
+                // don't have data, so use the error_message if there is one
+                val msg = r.error_message.getOrElse("Unkown Error.")
+                (new Exception(msg)).asLeft
+              }
+            }
+          }
+          .leftMap(e =>
+            // e could be from an exception in client or deserialization, or from an error in the response data
+            new Exception(
+              s"Error getting elevation from Google: ${e.getMessage}"))
+      }
   }
 }
